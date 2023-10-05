e0f3b956494689ccc179a3ea0c9d1e5d2a5d2b02
Enes Batur <enay10171@gmail.com>
2023-10-05 20:33:40 +0300

    ChatGPT v1.0

---
+++ src/main/java/chatgpt/chatGPT.java
@@ -0,0 +1,71 @@
+package chatgpt;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+
+public class chatGPT {
+    public static void main(String[] args) {
+        System.out.println(chatGPT("Atatürk kimdir"));
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+    public static String chatGPT(String message){
+
+        String url = "https://api.openai.com/v1/chat/completions";
+        String apiKey="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";
+        String model="gpt-3.5-turbo";
+        try {
+
+            URL obj = new URL(url);
+            HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+            con.setRequestMethod("POST");
+            con.setRequestProperty("Authorization", "Bearer " + apiKey);
+            con.setRequestProperty("Content-Type", "application/json");
+
+            // Build the request body
+            String body = "{\"model\": \"" + model + "\", \"messages\": [{\"role\": \"user\", \"content\": \"" + message + "\"}]}";
+            con.setDoOutput(true);
+            OutputStreamWriter writer = new OutputStreamWriter(con.getOutputStream());
+            writer.write(body);
+            writer.flush();
+            writer.close();
+
+            // Get the response
+            BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+            String inputLine;
+            StringBuffer response = new StringBuffer();
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+            in.close();
+
+            // returns the extracted contents of the response.
+            return extractContentFromResponse(response.toString());
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    // This method extracts the response expected from chatgpt and returns it.
+    public static String extractContentFromResponse(String response) {
+        int startMarker = response.indexOf("content")+11; // Marker for where the content starts.
+        int endMarker = response.indexOf("\"", startMarker); // Marker for where the content ends.
+        return response.substring(startMarker, endMarker); // Returns the substring containing only the response.
+    }
+}
