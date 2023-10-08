622e08571a27fc5501694496ed905f8216e34fd8
Enes Batur <enay10171@gmail.com>
2023-10-08 23:44:42 +0300

    ChatGPT v1.2

---
+++ src/main/java/chatgpt/d01IOExcept.java
@@ -0,0 +1,12 @@
+package chatgpt;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class d01IOExcept {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int a = input.nextInt();
+
+    }
+}
